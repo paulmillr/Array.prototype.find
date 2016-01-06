7947bec5ea09f8ebb6ebec55af632aece715d59a
Jake Champion <jakechampion.jake2@googlemail.com>
2016-01-06 18:52:25 +0000

    Apply changes made from editor config file

--- implementation.js
+++ implementation.js
@@ -3,17 +3,17 @@
 'use strict';
 var ES = require('es-abstract/es6');
 
-module.exports = function find (predicate) {
-  var list = ES.ToObject(this);
-  var length = ES.ToUint32(ES.ToLength(list.length));
-  if (length === 0) return undefined;
-  if (!ES.IsCallable(predicate)) {
-    throw new TypeError('Array#find: predicate must be a function');
-  }
-  var thisArg = arguments[1];
-  for (var i = 0, value; i < length; i++) {
-    value = list[i];
-    if (ES.Call(predicate, thisArg, [value, i, list])) return value;
-  }
-  return undefined;
+module.exports = function find(predicate) {
+	var list = ES.ToObject(this);
+	var length = ES.ToUint32(ES.ToLength(list.length));
+	if (length === 0) return undefined;
+	if (!ES.IsCallable(predicate)) {
+		throw new TypeError('Array#find: predicate must be a function');
+	}
+	var thisArg = arguments[1];
+	for (var i = 0, value; i < length; i++) {
+		value = list[i];
+		if (ES.Call(predicate, thisArg, [value, i, list])) return value;
+	}
+	return undefined;
 };
--- tests/test.js
+++ tests/test.js
@@ -3,105 +3,105 @@ var expect = require("chai").expect;
 
 var runTests = function (arrayFind) {
 
-  describe('Array', function(){
+	describe('Array', function(){
 
-      var list = [5, 10, 15, 20];
+			var list = [5, 10, 15, 20];
 
-      describe('#find', function() {
-        it('should have a length of 1', function() {
-          expect(arrayFind.length).to.equal(1);
-        });
+			describe('#find', function() {
+				it('should have a length of 1', function() {
+					expect(arrayFind.length).to.equal(1);
+				});
 
-        it('should find item by predicate', function() {
-          var result = arrayFind.call(list, function(item) { return item === 15; });
-          expect(result).to.equal(15);
-        });
+				it('should find item by predicate', function() {
+					var result = arrayFind.call(list, function(item) { return item === 15; });
+					expect(result).to.equal(15);
+				});
 
-        it('should return undefined when nothing matched', function() {
-          var result = arrayFind.call(list, function(item) { return item === 'a'; });
-          expect(result).to.equal(undefined);
-        });
+				it('should return undefined when nothing matched', function() {
+					var result = arrayFind.call(list, function(item) { return item === 'a'; });
+					expect(result).to.equal(undefined);
+				});
 
-        it('should throw TypeError when function was not passed', function() {
-          expect(function() { list.find(); }).to.throw(TypeError);
-        });
+				it('should throw TypeError when function was not passed', function() {
+					expect(function() { list.find(); }).to.throw(TypeError);
+				});
 
-        it('should receive all three parameters', function() {
-          var index = arrayFind.call(list, function(value, index, arr) {
-            expect(list[index]).to.equal(value);
-            expect(list).to.eql(arr);
-            return false;
-          });
-          expect(index).to.equal(undefined);
-        });
+				it('should receive all three parameters', function() {
+					var index = arrayFind.call(list, function(value, index, arr) {
+						expect(list[index]).to.equal(value);
+						expect(list).to.eql(arr);
+						return false;
+					});
+					expect(index).to.equal(undefined);
+				});
 
-        it('should work with the context argument', function() {
-          var context = {};
-          arrayFind.call([1], function() { expect(this).to.equal(context); }, context);
-        });
+				it('should work with the context argument', function() {
+					var context = {};
+					arrayFind.call([1], function() { expect(this).to.equal(context); }, context);
+				});
 
-        it('should work with an array-like object', function() {
-          var obj = { '0': 1, '1': 2, '2': 3, length: 3 };
-          var found = arrayFind.call(obj, function(item) { return item === 2; });
-          expect(found).to.equal(2);
-        });
+				it('should work with an array-like object', function() {
+					var obj = { '0': 1, '1': 2, '2': 3, length: 3 };
+					var found = arrayFind.call(obj, function(item) { return item === 2; });
+					expect(found).to.equal(2);
+				});
 
-        it('should work with an array-like object with negative length', function() {
-          var obj = { '0': 1, '1': 2, '2': 3, length: -3 };
-          var found = arrayFind.call(obj, function(item) {
-            throw new Error('should not reach here');
-          });
-          expect(found).to.equal(undefined);
-        });
+				it('should work with an array-like object with negative length', function() {
+					var obj = { '0': 1, '1': 2, '2': 3, length: -3 };
+					var found = arrayFind.call(obj, function(item) {
+						throw new Error('should not reach here');
+					});
+					expect(found).to.equal(undefined);
+				});
 
-        it('should work with a sparse array', function() {
-          var obj = [1, , undefined];
-          expect(1 in obj).to.equal(false);
-          var seen = [];
-          var found = arrayFind.call(obj, function(item, idx) {
-            seen.push([idx, item]);
-            return false;
-          });
-          expect(found).to.equal(undefined);
-          expect(seen).to.eql([[0, 1], [1, undefined], [2, undefined]]);
-        });
+				it('should work with a sparse array', function() {
+					var obj = [1, , undefined];
+					expect(1 in obj).to.equal(false);
+					var seen = [];
+					var found = arrayFind.call(obj, function(item, idx) {
+						seen.push([idx, item]);
+						return false;
+					});
+					expect(found).to.equal(undefined);
+					expect(seen).to.eql([[0, 1], [1, undefined], [2, undefined]]);
+				});
 
-        it('should work with a sparse array-like object', function() {
-          var obj = { '0': 1, '2': undefined, length: 3.2 };
-          var seen = [];
-          var found = arrayFind.call(obj, function(item, idx) {
-            seen.push([idx, item]);
-            return false;
-          });
-          expect(found).to.equal(undefined);
-          expect(seen).to.eql([[0, 1], [1, undefined], [2, undefined]]);
-        });
-      });
-  });
+				it('should work with a sparse array-like object', function() {
+					var obj = { '0': 1, '2': undefined, length: 3.2 };
+					var seen = [];
+					var found = arrayFind.call(obj, function(item, idx) {
+						seen.push([idx, item]);
+						return false;
+					});
+					expect(found).to.equal(undefined);
+					expect(seen).to.eql([[0, 1], [1, undefined], [2, undefined]]);
+				});
+			});
+	});
 };
 
 var find = require('../index');
 
 describe('polyfill', function() {
-  describe('clean Object.prototype', function() {
-    runTests(find.implementation);
-  });
+	describe('clean Object.prototype', function() {
+		runTests(find.implementation);
+	});
 
-  describe('polluted Object.prototype', function() {
-    Object.prototype[1] = 42;
-    runTests(find.implementation);
-    delete Object.prototype[1];
-  });
+	describe('polluted Object.prototype', function() {
+		Object.prototype[1] = 42;
+		runTests(find.implementation);
+		delete Object.prototype[1];
+	});
 });
 
 describe('shim', function() {
-  describe('clean Object.prototype', function() {
-    runTests(find.implementation);
-  });
+	describe('clean Object.prototype', function() {
+		runTests(find.implementation);
+	});
 
-  describe('polluted Object.prototype', function() {
-    Object.prototype[1] = 42;
-    runTests(find.implementation);
-    delete Object.prototype[1];
-  });
+	describe('polluted Object.prototype', function() {
+		Object.prototype[1] = 42;
+		runTests(find.implementation);
+		delete Object.prototype[1];
+	});
 });
