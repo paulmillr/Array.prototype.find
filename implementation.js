5c617317882ab28179e0767a489eb65b6083ed06
Jake Champion <jakechampion.jake2@googlemail.com>
2016-01-06 17:38:46 +0000

    Uncallable predicates must throw even when the array is empty.

--- implementation.js
+++ implementation.js
@@ -5,11 +5,11 @@ var ES = require('es-abstract/es6');
 
 module.exports = function find(predicate) {
 	var list = ES.ToObject(this);
-	if (length === 0) return undefined;
 	var length = ES.ToInteger(ES.ToLength(list.length));
 	if (!ES.IsCallable(predicate)) {
 		throw new TypeError('Array#find: predicate must be a function');
 	}
+	if (length === 0) return undefined;
 	var thisArg = arguments[1];
 	for (var i = 0, value; i < length; i++) {
 		value = list[i];
