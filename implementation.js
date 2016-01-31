e933ffa4053f41d91c2902b2ce8a8060027f237c
Jordan Harband <ljharb@gmail.com>
2016-01-31 14:26:51 -0800

    Remove redundant licensing and documentation information, since it’s in `package.json`
    Closes #14.

--- implementation.js
+++ implementation.js
@@ -1,6 +1,5 @@
-// Array.prototype.find - MIT License (c) 2013 Paul Miller <http://paulmillr.com>
-// For all details and docs: https://github.com/paulmillr/array.prototype.find
 'use strict';
+
 var ES = require('es-abstract/es6');
 
 module.exports = function find(predicate) {
