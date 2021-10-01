aeef5df455bc27c18823b0b7a3bfb1dbed7addb0
Jordan Harband <ljharb@gmail.com>
2021-09-30 23:29:42 -0700

    [Deps] use `call-bind` for Array slice

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 'use strict';
 
 var define = require('define-properties');
+var callBound = require('call-bind/callBound');
 var RequireObjectCoercible = require('es-abstract/2021/RequireObjectCoercible');
 
 var implementation = require('./implementation');
 var getPolyfill = require('./polyfill');
 var shim = require('./shim');
 
-var slice = Array.prototype.slice;
+var $slice = callBound('Array.prototype.slice');
 
 var polyfill = getPolyfill();
 
 var boundFindShim = function find(array, predicate) { // eslint-disable-line no-unused-vars
 	RequireObjectCoercible(array);
-	var args = slice.call(arguments, 1);
+	var args = $slice(arguments, 1);
 	return polyfill.apply(array, args);
 };
 
